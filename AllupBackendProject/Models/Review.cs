52477e08e6ad9c57694cce59a468192098e56a0d
RashidRMa <rashidrma@code.edu.az>
2022-07-25 02:37:00 +0400

    feat:"reviews added"

--- AllupBackendProject/Models/Review.cs
+++ AllupBackendProject/Models/Review.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string SenderName { get; set; }
         public string SenderEmail { get; set; }
-        public int SendText { get; set; }
+        public string SendText { get; set; }
 
         public int ProductId { get; set; }
         public Product Products { get; set; }
